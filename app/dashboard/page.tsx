e903ffa02081770ffb5e0ca70ffe7872f6287059
adamrichardson14 <adamrichardson14@icloud.com>
2023-04-06 23:13:53 +0300

    Initial Commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import SignOut from "@/components/sign-out";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>This is the dashboard</h1>
+
+      <SignOut />
+    </div>
+  );
+}
--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,8 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req: NextRequest) {
-  // Get the pathname of the request (e.g. /, /protected)
   const path = req.nextUrl.pathname;
-  
-  // If it's the root path, just render it
+
   if (path === "/") {
     return NextResponse.next();
   }
@@ -14,10 +12,10 @@ export default async function middleware(req: NextRequest) {
     req,
     secret: process.env.NEXTAUTH_SECRET,
   });
-  if (!session && path === "/protected") {
+  if (!session && path === "/dashboard") {
     return NextResponse.redirect(new URL("/login", req.url));
   } else if (session && (path === "/login" || path === "/register")) {
-    return NextResponse.redirect(new URL("/protected", req.url));
+    return NextResponse.redirect(new URL("/dashboard", req.url));
   }
   return NextResponse.next();
 }
